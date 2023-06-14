b895ec81e25b88e24f53ebee39a701bb72ef181b
lenglee <lengleevaja@gmail.com>
2023-06-14 09:09:53 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import "./App.css";
+import { useState } from 'react';
+//import Tasks from "./components/Tasks";
+import axios from "axios";
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+  const auth = getAuth();
+
+  const [authorizedUser, setAuthorizedUser] = useState(
+    false || sessionStorage.getItem("accessToken")
+  );
+
+  // function signInwithGoogle() {
+  //   signInWithPopup(auth, provider)
+  //     .then((result) => {
+  //       const user = result.user;
+        
+  //       if (user) {
+  //         user.getIdToken().then((idToken) => {
+  //           // set access token in session storage
+  //           sessionStorage.setItem("accessToken", idToken);
+  //           setAuthorizedUser(true);
+            
+  //           // Send the idToken to the backend
+  //           axios
+  //             .post("http://localhost:3007/api/auth/google-login", { idToken })
+  //             .then((response) => {
+  //               console.log(response);
+  //             })
+  //             .catch((error) => {
+  //               // Handle error response from the backend
+  //               console.log(error);
+  //             });
+  //         });
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }
+  function signInwithGoogle() {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+  
+        if (user) {
+          user.getIdToken().then((idToken) => {
+            // set access token in session storage
+            sessionStorage.setItem("accessToken", idToken);
+            setAuthorizedUser(true);
+  
+            // Send the idToken to the backend using axios
+            const url = 'http://localhost:8080/home';
+            const headers = { 'Content-Type': 'application/json' };
+            const data = {
+              query: `
+                mutation Mutation($data: IdToken) {
+                  signInWithGoogle(data: $data) {
+                    accessToken
+                  }
+                }
+              `,
+              variables: {
+                data: {
+                  idToken: idToken,
+                },
+              },
+            };
+  
+            axios.post(url, data, { headers })
+              .then((response) => {
+                console.log(response);
+              })
+              .catch((error) => {
+                // Handle error response from the backend
+                console.log(error);
+              });
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  function logoutUser() {
+    signOut(auth)
+      .then(() => {
+        // clear session storage
+        sessionStorage.clear();
+        setAuthorizedUser(false);
+        alert('Logged Out Successfully');
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {authorizedUser ? (
+        <>
+          <p>Authorized user</p>
+          <h1>Tasks</h1>
+          {/* <Tasks token={sessionStorage.getItem("accessToken")} /> */}
+          <button onClick={logoutUser}>Logout Button</button>
+        </>
+      ) : (
+        <>
+          <button onClick={signInwithGoogle}>SignWithGoogle</button>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/config/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSHn3b0sSxN7gj4tiX9fNHHpe2fIPwgXY",
+  authDomain: "fir-54046.firebaseapp.com",
+  projectId: "fir-54046",
+  storageBucket: "fir-54046.appspot.com",
+  messagingSenderId: "619508603984",
+  appId: "1:619508603984:web:0c6845dbdbb36c794b8555"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
